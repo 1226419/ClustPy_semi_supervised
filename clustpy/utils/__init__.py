985c64947c82250554691dbf9c723092a0b7ea0f
christophl16 <christoph.loitzenbauer@univie.ac.at>
2024-04-08 13:28:22 +0200

    updated init to include packages from upstream updates

--- clustpy/utils/__init__.py
+++ clustpy/utils/__init__.py
@@ -1,5 +1,5 @@
 from .evaluation import evaluate_dataset, evaluate_multiple_datasets, EvaluationDataset, EvaluationAlgorithm, \
-    EvaluationMetric
+    EvaluationMetric, EvaluationAutoencoder, load_saved_autoencoder, evaluation_df_to_latex_table
 from .plots import plot_with_transformation, plot_image, plot_scatter_matrix, plot_histogram, plot_1d_data, \
     plot_2d_data, plot_3d_data
 
