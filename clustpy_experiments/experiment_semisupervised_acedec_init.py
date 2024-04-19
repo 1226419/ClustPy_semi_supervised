aac9fceddd8f24165da157291afbe054e7460c37
christophl16 <christoph.loitzenbauer@univie.ac.at>
2024-04-19 11:39:10 +0200

    different amount of labeled data test for init

--- clustpy_experiments/experiment_semisupervised_acedec_init.py
+++ clustpy_experiments/experiment_semisupervised_acedec_init.py
@@ -19,3 +19,22 @@ def experiment_semi_init_small_feedforward(datasets, algorithms_function, metric
                 n_pretrain_epochs, n_clustering_epochs, optimizer_class, pretrain_optimizer_params, loss_fn,
                 ae_class, other_ae_params, datasets, algorithms_function, metrics, save_dir, download_path,
                 train_labels_percent=70)
+
+
+def experiment_semi_init_small_feedforward_multiple_label_percent(datasets, algorithms_function, metrics, save_dir, download_path,
+                                          n_repetitions=10, batch_size=256, n_pretrain_epochs=100,
+                                          n_clustering_epochs=150,
+                                          optimizer_class=torch.optim.Adam,
+                                          pretrain_optimizer_params={"lr": 1e-3}, loss_fn=torch.nn.MSELoss(),
+                                          other_ae_params={}):
+    experiment_base_name = "semisupervised_init_small_ff_labels"
+    embedding_size = 10
+    ae_layers = [512, 256, 128]
+    ae_class = FeedforwardAutoencoder
+    labels_percent_list = [0, 10, 30, 50, 70, 90, 100]
+    for labels in labels_percent_list:
+        experiment_name = experiment_base_name + "_" + str(labels)
+        _experiment(experiment_name, ae_layers, embedding_size, n_repetitions, batch_size,
+                    n_pretrain_epochs, n_clustering_epochs, optimizer_class, pretrain_optimizer_params, loss_fn,
+                    ae_class, other_ae_params, datasets, algorithms_function, metrics, save_dir, download_path,
+                    train_labels_percent=labels)
